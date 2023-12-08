c6874c185f18d76a73a1b500a84a2388c1feacff
rishulpu1 <rishulpu1@gmail.com>
2023-12-08 11:53:01 +0530

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,7 @@
+export default function rootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.jsx
@@ -0,0 +1,3 @@
+export default function homePage() {
+  return <h1>This is my first page</h1>;
+}
