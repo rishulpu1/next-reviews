f07f1377d6ed32b06f3368bca6b4c3a49b09fe47
rishulpu1 <rishulpu1@gmail.com>
2023-12-09 19:24:14 +0530

    created 2 static pages

---
+++ app/about/page.jsx
@@ -0,0 +1,43 @@
+export default function About() {
+  return (
+    <>
+      <h1>About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis dolore
+        assumenda tempore pariatur, aliquam cupiditate! Aperiam cumque nostrum
+        in rem sint. Veniam quibusdam at eum possimus, in inventore facere
+        laboriosam dicta. Exercitationem distinctio delectus nesciunt nobis
+        obcaecati iste! Quod hic expedita aspernatur cumque dignissimos. Dolore
+        culpa vitae excepturi suscipit alias nisi est rerum cum! Maiores iure
+        facilis similique eum hic omnis accusamus quibusdam iste aut laboriosam
+        animi soluta error, nobis corrupti magni officiis tenetur tempore nihil
+        explicabo veritatis? A voluptas quisquam quidem quasi, dolorem
+        accusantium magni tenetur tempore obcaecati ipsum, totam et id eum
+        debitis? Repellendus pariatur necessitatibus odio sit rem, corrupti ea
+        nisi doloribus earum ipsam ipsa amet repudiandae delectus eius quisquam
+        quod cumque quasi dicta magni, voluptate itaque quibusdam dolorem aut.
+        Dolore soluta aliquid itaque quae? Id officia eum atque, magnam incidunt
+        eligendi adipisci nisi ipsam voluptatem ex fugit rerum distinctio,
+        expedita facere, doloribus excepturi totam saepe hic dolor sequi?
+        Repudiandae rerum nihil deleniti quisquam facere placeat eligendi nulla
+        laudantium nam sapiente, tempore aperiam animi praesentium dicta fuga
+        error eaque sed esse labore. Blanditiis laudantium magnam libero dolores
+        harum quis necessitatibus. Architecto, beatae, laborum autem fugiat quia
+        esse dolores quasi consequuntur vitae, maiores enim vero eos expedita
+        molestias iste eveniet nisi optio? Labore praesentium consequuntur in,
+        explicabo qui dolor eveniet facilis consequatur eligendi veritatis optio
+        accusantium provident velit recusandae dolorem tenetur perspiciatis
+        sapiente vero sed, aut exercitationem doloremque itaque. Reiciendis
+        accusamus mollitia possimus dignissimos illo illum, ad ea provident
+        distinctio reprehenderit corrupti numquam, alias natus praesentium
+        cupiditate maiores consequuntur quibusdam laboriosam facere molestiae
+        sequi rerum labore. Sint deserunt totam molestiae recusandae obcaecati
+        quibusdam asperiores iste itaque, molestias voluptatum aspernatur
+        similique corporis dolor quia ipsum temporibus officia. Voluptatibus
+        animi eius qui est blanditiis dolorem rerum optio earum sapiente
+        eveniet. Repellendus laborum dolores laudantium temporibus doloremque
+        qui magnam asperiores sed.
+      </p>
+    </>
+  );
+}
